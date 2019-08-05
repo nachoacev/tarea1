d8136b9ac092e4e08b9c3c17359bb03d51fc5242
Ignacio Acevedo <ignacio.acevedo.r@outlook.com>
2019-08-05 00:18:18 -0400

    Funcion integral sin depurar

--- pregunta2.py
+++ pregunta2.py
@@ -3,10 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt 
 
 def function(u,phi):            # funcion integrando
-    denominador = math.sqrt(math.cos(phi)-math.cos(phi-(u**2)))
-    return u/denominador
 
-def simpson_1_3(phi,N):         # aproximacion simpson 1/3, N par
+    denominador = math.cos(phi-(u**2))-math.cos(phi)
+    return u/math.sqrt(denominador)
+
+def simpson_1_3(phi,N):         # aproximacion simpson 1/3, N impar
 
     # valor del paso
     h = math.sqrt(phi)/N        
@@ -15,9 +16,11 @@ def simpson_1_3(phi,N):         # aproximacion simpson 1/3, N par
     x = []
     fx = []
 
-    while i <= N:
-        x.append(i*h)
-        fx.append(function(x[i]))
+    i = 0
+    fx.append(0)
+    while i <= N-1:
+        x.append((i+1)*h)
+        fx.append(function(x[i],phi))
         i += 1
 
     # calcular integral
@@ -28,3 +31,19 @@ def simpson_1_3(phi,N):         # aproximacion simpson 1/3, N par
             int += fx[i]
         elif i % 2 != 0:
             int += 4*fx[i]
+        else:
+            int += 2*fx[i]
+        k += 1
+    return int*h/3
+
+phi0 = math.pi/4
+
+T_T0 = (2*math.sqrt(2)/math.pi)*simpson_1_3(phi0,100000)
+T_T0
+
+#function(0.1,phi0)
+
+#a = math.cos(phi0)-math.cos(phi0-0.001**2)
+#0.001/math.sqrt(a)
+#a
+#pkill -9 python
