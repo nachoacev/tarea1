a6746492dc9930348163622b15f01964440d1e98
Ignacio Acevedo <ignacio.acevedo.r@outlook.com>
2019-08-07 00:44:04 -0400

    P1 hecha, P2 parte 1 hecha

--- pregunta1.py
+++ pregunta1.py
@@ -2,31 +2,59 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-t = 1.258  # punto a estimar
-h = np.logspace(-1,-15,15,base=10.0)
+h = np.logspace(-1,-15,15,base=10.0)         # diferentes h's float64
+H = np.float32(np.logspace(-1,-15,15,base=10.0)) # diferentes h's float32
 
-diff_simple = []
-diff_mejor = []
+diff_simple_mejor64 = [[],[]]
+#diff_mejor64 = []
 
-def f_util(x):                     # funcion a estudiar
+diff_simple_mejor32 = [[],[]]
+#diff_mejor32 = []
+
+t = 1.258
+valor_exacto = math.cos(1.258/2)/2
+T = np.float32(np.asarray([t]))
+
+
+def f_util(x):
     return math.sin(x/2)
 
-valor_exacto = math.cos(1.258/2)/2 # valor mas cerca al teorico
+def aprox1_4(t,h):                         #aproximacion orden 1 y 4
+    return [(f_util(t+h)-f_util(t))/h, (-f_util(t+2*h)+8*f_util(t+h)-8*f_util(t-h)+f_util(t-2*h))/(12*h)]
 
-for i in range(15):                # lista con diff de derivada teori y aproxim
-    df1 = (-f_util(t+2*h[i])+8*f_util(t+h[i])-8*f_util(t-h[i])+f_util(t-2*h[i]))/(12*h[i])
-    df2 = (f_util(t+h[i])-f_util(t))/h[i]
-    diff_mejor.append(abs(df1-valor_exacto))
-    diff_simple.append(abs(df2-valor_exacto))
+#def aprox4(t,h):                         #aproximacion orden 4
+ #   return (-f_util(t+2*h)+8*f_util(t+h)-8*f_util(t-h)+f_util(t-2*h))/(12*h)
 
-# ploteo
-plt.plot(h, diff_mejor, color='g', label="Derivada simetrica")
-plt.plot(h, diff_simple, color='b', label="Derivada simple")
-plt.xscale('log')
+for i in range(len(h)):                   #añadir diff a listas
+    df_64 = aprox1_4(t,h[i])              #diff con floats 64
+    df_32 = aprox1_4(T[0],H[i])             #diff con floats 32
+    
+    #agregar
+
+    diff_simple_mejor32[0].append(np.abs(df_32[0]-valor_exacto))
+    diff_simple_mejor32[1].append(np.abs(df_32[1]-valor_exacto))
+    diff_simple_mejor64[0].append(np.abs(df_64[0]-valor_exacto))
+    diff_simple_mejor64[1].append(np.abs(df_64[1]-valor_exacto))
+
+plt.plot(h, diff_simple_mejor64[1], color='g', label="Derivada simetrica")
+plt.plot(h, diff_simple_mejor64[0], color='b', label="Derivada simple")
 plt.yscale('log')
+plt.xscale('log')
 #plt.ylim(1e-12, 1e-1)
-#plt.axhline(0)
+plt.axhline(0)
 plt.xlabel('h', fontsize=20)
 plt.ylabel('$\\frac{d}{dx}sin(x/2) - \\frac{1}{2}cos(x/2)$', fontsize=20)
 plt.legend(loc='lower left')
+plt.title('float64')
 #_ = plt.xticks(h[::2])
+
+plt.plot(h, diff_simple_mejor32[1], color='g', label="Derivada simetrica")
+plt.plot(h, diff_simple_mejor32[0], color='b', label="Derivada simple")
+plt.yscale('log')
+plt.xscale('log')
+#plt.ylim(1e-12, 1e-1)
+plt.axhline(0)
+plt.xlabel('h', fontsize=20)
+plt.ylabel('$\\frac{d}{dx}sin(x/2) - \\frac{1}{2}cos(x/2)$', fontsize=20)
+plt.legend(loc='lower left')
+plt.title('float32')
--- pregunta2.py
+++ pregunta2.py
@@ -2,48 +2,63 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt 
 
+# Determinacion de algoritmo simpson para el integrando
+
 def function(u,phi):            # funcion integrando
 
     denominador = math.cos(phi-(u**2))-math.cos(phi)
     return u/math.sqrt(denominador)
 
-def simpson_1_3(phi,N):         # aproximacion simpson 1/3, N impar
+def trapecio(phi,N):            # aproximacion por trapecio, N entero
 
     # valor del paso
-    h = math.sqrt(phi)/N        
+    h = math.sqrt(phi)/N
 
-    # listas para guardar puntos y evaluacion
-    x = []
-    fx = []
-
-    i = 0
-    fx.append(0)
-    while i <= N-1:
-        x.append((i+1)*h)
-        fx.append(function(x[i],phi))
-        i += 1
-
-    # calcular integral
-    int = 0
-    k = 0
+    k = 1                         # no evaluamos el integrando en 0 porque se infedine
+    suma = 0                       # valor suma
     while k <= N:
-        if k == 0 or i == N:
-            int += fx[i]
-        elif i % 2 != 0:
-            int += 4*fx[i]
+        if k == N:                # no evaluamos el principio
+            suma += function(k*h,phi)
         else:
-            int += 2*fx[i]
+            suma += 2*function(k*h,phi)
         k += 1
-    return int*h/3
+    return suma*h/2
 
-phi0 = math.pi/4
+trapecio(math.pi/4,100)
 
-T_T0 = (2*math.sqrt(2)/math.pi)*simpson_1_3(phi0,100000)
-T_T0
+# uso de simpson para distintos phi's y N's de manera recursiva
+# simpson(2N) = (4*trapecio(2N)-trapecio(N))/3
 
-#function(0.1,phi0)
+phi = np.flip(np.linspace(math.pi/2,0,1000,endpoint=False))
 
-#a = math.cos(phi0)-math.cos(phi0-0.001**2)
-#0.001/math.sqrt(a)
-#a
-#pkill -9 python
+integrales = []             # guarda las integrales para distintos phi's
+
+#for i in range(1,len(phi)):
+#    N = 20
+#    simpson_2N = 0
+ #   simpson_4N = 1
+  #  error = 1
+   # while error > 0.01:
+    #    simpson_4N =  (4*trapecio(phi[i],2*N+2)-trapecio(phi[i],N+1))/3
+     #   error = math.fabs(simpson_2N-simpson_4N)/simpson_4N)
+      #  if error <= 0.01:
+       #     integrales.append(simpson_4N)
+        #else:
+         #   simpson_2N = simpson_4N
+          #  N = 2*N
+   # i += 1     
+
+for i in range(1,len(phi)):
+    N = 10
+    trap_N = 0
+    trap_2N = 0
+    trap_4N = 1
+    error = 1
+    p = phi[i]
+    while error > 0.01:
+        trap_N = trapecio(p,N)
+        trap_2N = trapecio(p,2*N)
+        trap_4N = trapecio(p, 4*N)
+        error = math.fabs((4*trap_4N-5*trap_2N+trap_N)/(4*trap_2N-trap_N))
+        N = 2*N
+    integrales.append((4*trap_4N-trap_2N)/3)
